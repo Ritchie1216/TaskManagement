a67494d65e40935f206167c7c0b6874cf655ed1e
ritchie121600@gmail.com <ritchie121600@gmail.com>
2024-10-08 13:30:25 +0800

    Adding layout header and footer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Task List')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome to Task Manager</h1>
+        <p>This is the main content of the index page.</p>
+
+        <div class="container">
+            <h1>Testing</h1>
+            <table>
+                <!-- 表格内容 -->
+            </table>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <!-- About section -->
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Task Manager</h5>
+                <p>
+                    A simple and efficient task management system to keep you organized.
+                </p>
+            </div>
+
+           
+       
+    <!-- Copyright section -->
+    <div class="text-center p-3 bg-dark text-white">
+        © 2024 Task Manager. All rights reserved.
+    </div>
+</footer>
